f1e3a3d276fdcd9e2eb8bd558dbae54b6afd9cfd
Simoncao <13671707129@126.com>
2021-08-23 15:37:45 +0800

    [feat]ssh func

---
+++ go.mod
@@ -0,0 +1,5 @@
+module arubaStatusPrometheus
+
+go 1.16
+
+require github.com/melbahja/goph v1.2.1
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+
+	"github.com/melbahja/goph"
 )
 
 func main() {
+	//获取命令行参数
 	var host, user, password string
 	var httpPort int
 
@@ -13,7 +17,21 @@ func main() {
 	flag.StringVar(&user, "u", "root", "user 默认为root")
 	flag.StringVar(&password, "p", "", "password 默认为空")
 	flag.IntVar(&httpPort, "P", 9100, "port 默认为9100")
-
 	flag.Parse()
-	fmt.Print(host, ",", user, ",", password, ",", httpPort)
+
+	//登陆aruba交换机
+	client, err := goph.New(user, host, goph.Password(password))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer client.Close()
+
+	res, err := client.Run("show cpuinfo")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(res)
+
 }
